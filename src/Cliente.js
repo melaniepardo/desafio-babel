d2b5f053a400ac2bef230b8531d5bdc0028ec66d
melaniepardo <melaniepardoes@gmail.com>
2022-03-05 13:02:01 -0300

    actualizacion

--- src/Cliente.js
+++ src/Cliente.js
@@ -1,5 +1,3 @@
-import Impuestos from "./Impuestos.js"
-
 export default class Cliente {
     constructor(nombre, impuestos) {
         this._nombre = nombre
--- src/main.js
+++ src/main.js
@@ -3,5 +3,5 @@ import Impuestos from './Impuestos'
 
 const impuesto1 = new Impuestos(1000, 2000)
 
-const cliente1 = new Cliente(José, impuesto1)
+const cliente1 = new Cliente("José", impuesto1)
 console.log(cliente1.calcularImpuesto)
